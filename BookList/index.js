b347b0fa38221a9358a66f14f917d5c06e2e99f8
Roberto Martinez <roberto_martinez0815@hotmail.com>
2025-02-11 13:53:58 -0600

    EJS habilitado con informacion de base de datos

--- BookList/index.js
+++ BookList/index.js
@@ -4,11 +4,21 @@ import pg from 'pg';
 const app = express()
 const port = 3000
 
+const client = new pg.Client({
+  user: 'postgres',
+  password: 'DckzDB15!',
+  host: 'localhost',
+  port: 5432,
+  database: 'books',
+})
+client.connect();
 
+
+let result = await client.query('SELECT * FROM book_list')
 
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs',{ data : result.rows})
   })
   
   app.listen(port, () => {
